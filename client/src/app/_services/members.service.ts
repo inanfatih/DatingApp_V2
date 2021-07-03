14ee14a1cc18de14994d31a13ede5771f85afeb1
Fatih <mfatihinan@gmail.com>
2021-07-02 20:42:53 -0400

    members section is completed

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+import { Member } from './../_models/member';
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
+
+  getMembers(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.baseUrl + 'users');
+  }
+
+  getMember(username: string): Observable<Member> {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+}
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,24 @@
+import { MembersService } from './../../_services/members.service';
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  members: Member[];
+  constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+  loadMembers(): void {
+    this.memberService.getMembers().subscribe((members) => {
+      this.members = members;
+      console.log(`members`, members);
+    });
+  }
 }
